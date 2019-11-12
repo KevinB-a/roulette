d29a170876cde13963a0b70999834921499ed715
Kevin <kevin.billet.pro@gmail.com>
2019-11-12 14:21:32 +0100

    add user choice function

--- functions.py
+++ functions.py
@@ -9,19 +9,20 @@ def name_choice():
         user=input("please enter your name or pseudo")
         return username
     print("welcome on casino game ",username," !")
-#def user_choice():
+
+def user_choice():
     """the function allow the player to choose a number between 0 and 49 ,
     if enter his not a number or enter his str return to choice"""
-#    number_choice=50 #for enter in a loop
-#    while number_choice < 0 or number_choice > 49:
-#        try:
-#            number_choice=int(input("enter number between 0 and 49 :")) #ask user a number and convert it in integer
-#        except ValueError: # if number_choice not a number
-#            print("your enter is not a number") #display error message
-#            number_choice = 50 #return in a loop
-#        if number_choice < 0 or number_choice >49:
-#            print("your enter is not included in range") #display error message  if number is out of range
-#        return number_choice
+    number_choice=50 #for enter in a loop
+    while number_choice < 0 or number_choice > 49:
+        try:
+            number_choice=int(input("enter number between 0 and 49 :")) #ask user a number and convert it in integer
+        except ValueError: # if number_choice not a number
+            print("your enter is not a number") #display error message
+            number_choice = 50 #return in a loop
+        if number_choice < 0 or number_choice >49:
+            print("your enter is not included in range") #display error message  if number is out of range
+        return number_choice
 
 #def computer_choice():
 #    """this function makes it possible to randomly select values """
